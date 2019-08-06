6d1e9cabc1fef4ca84d895cc0dfde01c832bcb26
François Parmentier <francois.parmentier@gmail.com>
2019-08-06 17:05:12 +0200

    build(core): Add core package skeleton
    BREAKING CHANGE: Add the core package

---
+++ packages/core/__tests__/core.js
@@ -0,0 +1,6 @@
+'use strict';
+
+import * as CNS from '../src/core';
+
+describe('@ector/core', () => {
+});
---
+++ packages/core/src/core.js
@@ -0,0 +1,13 @@
+'use strict';
+
+export function addEntry(ector, entry) {
+
+}
+
+export function generateResponse(ector) {
+
+}
+
+export function getResponse(ector) {
+
+}
