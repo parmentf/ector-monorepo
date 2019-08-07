56004f981c7605a8579d35d44f993eef7eabc3d0
François Parmentier <francois.parmentier@gmail.com>
2019-08-07 10:06:18 +0200

    test(core): Add test for given bot name
    And lint the code.

--- packages/core/__tests__/core.js
+++ packages/core/__tests__/core.js
@@ -9,71 +9,96 @@ describe('@ector/core', () => {
         });
 
         it('should give default username', () => {
-            expect(ECTOR.addEntry({}, 'Test')).toHaveProperty('username', 'Guy');
+            expect(ECTOR.addEntry({}, 'Test')).toHaveProperty(
+                'username',
+                'Guy',
+            );
+        });
+
+        it('should accept given name', () => {
+            expect(ECTOR.addEntry({ name: 'Achille' }, 'Test')).toHaveProperty(
+                'name',
+                'Achille',
+            );
         });
     });
 
     describe('addEntry', () => {
         it('should add entry in the model', () => {
-            expect(ECTOR.addEntry({}, `Hello.`))
-            .toEqual({
+            expect(ECTOR.addEntry({}, `Hello.`)).toEqual({
                 name: 'ECTOR',
                 username: 'Guy',
                 cn: {
-                    node: [{
-                        label: 'sHello.',
-                        occ: 1
-                    }, {
-                        label: 'wHello.',
-                        occ: 1
-                    }],
-                    link: [{ from: 0, to: 1, coOcc: 1 }]
+                    node: [
+                        {
+                            label: 'sHello.',
+                            occ: 1,
+                        },
+                        {
+                            label: 'wHello.',
+                            occ: 1,
+                        },
+                    ],
+                    link: [{ from: 0, to: 1, coOcc: 1 }],
                 },
                 cns: {
                     Guy: {
                         'sHello.': { value: 100 },
-                        'wHello.': { value: 100 }
-                     }
+                        'wHello.': { value: 100 },
+                    },
                 },
                 lastSentenceLabel: 'sHello.',
-                lastTokenLabels: ['wHello.']
-            })
+                lastTokenLabels: ['wHello.'],
+            });
         });
 
         it('should link sequential tokens', () => {
-            expect(ECTOR.addEntry({}, `Hello ECTOR.`))
-            .toEqual({
+            expect(ECTOR.addEntry({}, `Hello ECTOR.`)).toEqual({
                 name: 'ECTOR',
                 username: 'Guy',
                 cn: {
-                    node: [{
-                        label: 'sHello {yourname}.',
-                        occ: 1
-                    }, {
-                        label: 'wHello',
-                        occ: 1
-                    }, {
-                        label: 'w{yourname}.',
-                        occ: 1
-                    }],
-                    link: [{
-                        from: 0, to: 1, coOcc: 1
-                     }, {
-                        from: 0, to: 2, coOcc: 1
-                    }, {
-                        from: 1, to: 2, coOcc: 1
-                    }]
+                    node: [
+                        {
+                            label: 'sHello {yourname}.',
+                            occ: 1,
+                        },
+                        {
+                            label: 'wHello',
+                            occ: 1,
+                        },
+                        {
+                            label: 'w{yourname}.',
+                            occ: 1,
+                        },
+                    ],
+                    link: [
+                        {
+                            from: 0,
+                            to: 1,
+                            coOcc: 1,
+                        },
+                        {
+                            from: 0,
+                            to: 2,
+                            coOcc: 1,
+                        },
+                        {
+                            from: 1,
+                            to: 2,
+                            coOcc: 1,
+                        },
+                    ],
                 },
                 cns: {
                     Guy: {
                         'sHello {yourname}.': { value: 100 },
-                        'wHello': { value: 100 },
+                        wHello: { value: 100 },
                         'w{yourname}.': { value: 100 },
-                     }
+                    },
                 },
                 lastSentenceLabel: 'sHello {yourname}.',
-                lastTokenLabels: ['wHello', 'w{yourname}.']
-            })
+                lastTokenLabels: ['wHello', 'w{yourname}.'],
+            });
         });
     });
 });
