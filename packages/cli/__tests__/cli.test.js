b6a6791ae2423aa3c95e6f5a395bd29037dbdc35
François Parmentier <francois.parmentier@gmail.com>
2019-08-22 20:23:25 +0200

    test(cli): Write a first test
    Test the --version argument

--- packages/cli/__tests__/cli.test.js
+++ packages/cli/__tests__/cli.test.js
@@ -1,8 +1,24 @@
 'use strict';
 
-const cli = require('../lib/cli');
+const tester = require('cli-tester');
+const { version } = require(`${__dirname}/../package.json`);
 
 describe('@ector/cli ector', () => {
-  // const argv = cli(cwd).parse(['args']);
-  it('needs tests');
+  it('--version should give version', () =>
+    tester(require.resolve('../bin/cli'), '--version')
+      .then(({ code, stdout, stderr }) => {
+        expect(code).toBe(0);
+        expect(stdout).toBe(version);
+        expect(stderr).toBe('');
+      })
+  );
+
+  it('-v should give version', () =>
+    tester(require.resolve('../bin/cli'), '-v')
+      .then(({ code, stdout, stderr }) => {
+        expect(code).toBe(0);
+        expect(stdout).toBe(version);
+        expect(stderr).toBe('');
+      })
+  );
 });
