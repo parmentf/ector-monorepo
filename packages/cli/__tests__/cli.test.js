4e1a2830d4b97ddcc69c848dfeaef295c889dbc7
François Parmentier <francois.parmentier@gmail.com>
2019-08-24 21:12:47 +0200

    style(cli): Lint

--- packages/cli/__tests__/cli.test.js
+++ packages/cli/__tests__/cli.test.js
@@ -4,29 +4,29 @@ const tester = require('cli-tester');
 const { version } = require(`${__dirname}/../package.json`);
 
 describe('@ector/cli ector', () => {
-  describe('version', () => {
-    it('--version should give version', () =>
-      tester(require.resolve('../bin/cli'), '--version')
-        .then(({ code, stdout, stderr }) => {
-          expect(code).toBe(0);
-          expect(stdout).toBe(version);
-          expect(stderr).toBe('');
-        })
-    );
+    describe('version', () => {
+        it('--version should give version', () =>
+            tester(require.resolve('../bin/cli'), '--version').then(
+                ({ code, stdout, stderr }) => {
+                    expect(code).toBe(0);
+                    expect(stdout).toBe(version);
+                    expect(stderr).toBe('');
+                },
+            ));
 
-    it('-v should give version', () =>
-      tester(require.resolve('../bin/cli'), '-V')
-        .then(({ code, stdout, stderr }) => {
-          expect(code).toBe(0);
-          expect(stdout).toBe(version);
-          expect(stderr).toBe('');
-        })
-    );
-  });
+        it('-v should give version', () =>
+            tester(require.resolve('../bin/cli'), '-V').then(
+                ({ code, stdout, stderr }) => {
+                    expect(code).toBe(0);
+                    expect(stdout).toBe(version);
+                    expect(stderr).toBe('');
+                },
+            ));
+    });
 
-  describe('setuser', () => {});
-  describe('setbot', () => {});
-  describe('reply', () => {});
-  describe('learn', () => {});
-  describe('about', () => {});
+    describe('setuser', () => {});
+    describe('setbot', () => {});
+    describe('reply', () => {});
+    describe('learn', () => {});
+    describe('about', () => {});
 });
