b8284eab7fe90bfe8f8f489bf500c17f9c1cf3a6
François Parmentier <francois.parmentier@gmail.com>
2019-08-24 11:12:18 +0200

    refactor(cli): Create a "version" description

--- packages/cli/__tests__/cli.test.js
+++ packages/cli/__tests__/cli.test.js
@@ -4,21 +4,23 @@ const tester = require('cli-tester');
 const { version } = require(`${__dirname}/../package.json`);
 
 describe('@ector/cli ector', () => {
-  it('--version should give version', () =>
-    tester(require.resolve('../bin/cli'), '--version')
-      .then(({ code, stdout, stderr }) => {
-        expect(code).toBe(0);
-        expect(stdout).toBe(version);
-        expect(stderr).toBe('');
-      })
-  );
+  describe('version', () => {
+    it('--version should give version', () =>
+      tester(require.resolve('../bin/cli'), '--version')
+        .then(({ code, stdout, stderr }) => {
+          expect(code).toBe(0);
+          expect(stdout).toBe(version);
+          expect(stderr).toBe('');
+        })
+    );
 
-  it('-v should give version', () =>
-    tester(require.resolve('../bin/cli'), '-V')
-      .then(({ code, stdout, stderr }) => {
-        expect(code).toBe(0);
-        expect(stdout).toBe(version);
-        expect(stderr).toBe('');
-      })
-  );
+    it('-v should give version', () =>
+      tester(require.resolve('../bin/cli'), '-V')
+        .then(({ code, stdout, stderr }) => {
+          expect(code).toBe(0);
+          expect(stdout).toBe(version);
+          expect(stderr).toBe('');
+        })
+    );
+  });
 });
