23f7727ed18d1f3ac81bf58852a20ec9edd9c3ca
François Parmentier <francois.parmentier@gmail.com>
2019-08-26 23:05:19 +0200

    feat(samples): Add en-bot sample

--- packages/samples/lib/samples.js
+++ packages/samples/lib/samples.js
@@ -1,6 +1,10 @@
 'use strict';
 
 module.exports = {
+    'en-bot': {
+        path: `${__dirname}/ector.en-bot.json`,
+        description: 'English, about bots',
+    },
     'fr-bot': {
         path: `${__dirname}/ector.fr-bot.json`,
         description: 'French, about bots',
